81558c354f9bb68aaf2f7eb6466e481b5131b33d
奥城健太郎 <ken09460308@gmail.com>
2019-04-29 13:16:19 +0900

    ver1.0

--- Location/ViewController.swift
+++ Location/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupLocationManager()
+    }
+    
+    //位置情報取得関数
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10000
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        latitude.text = String((location?.coordinate.latitude)!)
+        longitude.text = String((location?.coordinate.longitude)!)
     }
 
 
